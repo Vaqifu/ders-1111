713cdd6f256c69dd659078d115c503b3f53f83bb
Vaqifu <vagifyu@code.edu.az>
2022-06-02 11:54:59 +0400

    created

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user1 = new User("Usubzade");
+
+
+                user1.Password = "Salamatcilq1";
+
+            Console.WriteLine($"  {user1.UserName} Password {user1.Password}");
+
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class User
+    {
+         public User(string Username)
+        {
+            UserName = Username;
+        }
+
+        private string _username;
+        private string _password;
+
+     
+       
+        public string UserName  
+        {
+            get => _username;
+            set
+            {
+               
+                if (!String.IsNullOrEmpty(value)&& (value.Length>5) && value.Length<26 )
+                {
+                
+                    {
+                        _username = value;
+                    }
+
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (!String.IsNullOrEmpty(value) && (value.Length > 7) && (value.Length < 26) )
+                {
+                    int countDigit = 0;
+                    int countUpper = 0;
+                    int countLower = 0;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (Char.IsDigit(value[i]))
+                            countDigit++;
+                        else if (Char.IsUpper(value[i]))
+                            countUpper++;
+                        else if (Char.IsLower(value[i]))
+                            countLower++;
+
+                    }
+                    if (countDigit != 0 && countUpper != 0 && countLower != 0)
+                    {
+                        _password = value;  
+                    }
+                }
+            }
+        }
+    }
+}
